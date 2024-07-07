94cad51c2ef58c4195dfa463d84cf39feab4523d
none <shobonaoc@gmail.com>
2024-07-07 09:02:32 +0900

    delete routes.js

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -11,12 +11,19 @@ import Lara from "@primevue/themes/lara";
 import "primeicons/primeicons.css";
 
 import store from "@/store";
-import { routes } from "@/routes";
 import App from "@/App.vue";
+import ChatEntrance from "@/components/pages/ChatEntrance.vue";
+import RoomSelection from "@/components/pages/RoomSelection.vue";
+import ChatRoom from "@/components/pages/ChatRoom.vue";
 
 export const router = createRouter({
   history: createWebHashHistory(),
-  routes,
+  routes: [
+    { name: "entrance", path: "/", component: ChatEntrance },
+    { name: "select", path: "/select", component: RoomSelection },
+    { path: "/room", redirect: "/select" },
+    { name: "room", path: "/room/:id", component: ChatRoom },
+  ],
 });
 const LaraIndigo = definePreset(Lara, {
   semantic: {
